01a084314c21dc2a2b87bb77fd236cf5139c85d9
Anton <prradd@gmail.com>
2020-08-01 23:22:19 +0300

    before deploying to Heroku

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const clients = require("./routes/api/clients")
 
@@ -24,7 +25,17 @@ mongoose
 // Use Routes
 app.use('/api/clients', clients);
 
-const port = process.env.PORT || 7000;
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+        // Set static folder
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        })
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
